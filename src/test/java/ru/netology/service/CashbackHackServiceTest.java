1639bbd98e3c07d225333191e788eeb934bcfb6b
Maria test <bykova1985maria@gmail.com>
2024-02-15 20:54:20 +0700

    Added branch

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,10 +1,10 @@
 package ru.netology.service;
 
-import static org.testng.Assert.*;
+import static org.junit.Assert.*;
 
 public class CashbackHackServiceTest {
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemainCashback() {
         CashbackHackService service = new CashbackHackService();
         int amount = 900;
@@ -13,7 +13,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1000;
@@ -22,7 +22,7 @@ public class CashbackHackServiceTest {
         assertEquals(actual, expected);
     }
 
-    @org.testng.annotations.Test
+    @org.junit.Test
     public void testOverRemain() {
         CashbackHackService service = new CashbackHackService();
         int amount = 1001;
